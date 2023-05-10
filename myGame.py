b5db460ff576f4ae8540552924007cc444d3b9e2
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-10 23:32:24 +0200

    import game background

--- myGame.py
+++ myGame.py
@@ -6,8 +6,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+test_surface = pygame.image.load('graphics/Sky.png')
 
 while True:
     for event in pygame.event.get():
