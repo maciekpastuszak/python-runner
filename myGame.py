4feec65f63b665d1e9143b7fb31520b15ddb5cd1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-10 22:54:32 +0200

    add first event - quit

--- myGame.py
+++ myGame.py
@@ -2,3 +2,11 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    # draw all our elements
+    # update everything
+    pygame.display.update()
