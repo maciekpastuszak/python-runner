a057b8e6f0285e0c0e60d2430791a3122b03b2d7
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-10 23:02:35 +0200

    add max framing to 60

--- myGame.py
+++ myGame.py
@@ -4,6 +4,8 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -12,3 +14,4 @@ while True:
     # draw all our elements
     # update everything
     pygame.display.update()
+    clock.tick(60)
