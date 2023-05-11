eaec2cab26fd729e7dc05740178efd098e89e06c
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-11 09:07:41 +0200

    add basic snail animation

--- myGame.py
+++ myGame.py
@@ -11,15 +11,20 @@ sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = test_font.render('My game', False, 'Black')
 
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface,(300, 50))
-
+    snail_x_pos -= 4
+    if snail_x_pos < -100: snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos,250))
+        
     pygame.display.update()
     clock.tick(60)
