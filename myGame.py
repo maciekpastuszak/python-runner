ca49623b78d3781e94447ebc2a06b0313db4b192
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-11 09:17:05 +0200

    add player surface and convert imgs

--- myGame.py
+++ myGame.py
@@ -7,12 +7,14 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
+
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 
 while True:
     for event in pygame.event.get():
@@ -25,6 +27,7 @@ while True:
     snail_x_pos -= 4
     if snail_x_pos < -100: snail_x_pos = 800
     screen.blit(snail_surface,(snail_x_pos,250))
+    screen.blit(player_surf,(80,200))
         
     pygame.display.update()
     clock.tick(60)
