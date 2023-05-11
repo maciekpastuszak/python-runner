25294ec9fdf646b1627ecbd62d80205fe7b6630e
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-11 08:48:06 +0200

    add ground img and correct sky

--- myGame.py
+++ myGame.py
@@ -6,7 +6,8 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-test_surface = pygame.image.load('graphics/Sky.png')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 
 while True:
     for event in pygame.event.get():
@@ -14,7 +15,8 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface, (0, 0))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
 
     pygame.display.update()
     clock.tick(60)
