b507c8574071af697f35a29d35730d9ccc70b76f
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-12 14:39:13 +0200

    simulate collision to keep player on the ground when jumping

--- myGame.py
+++ myGame.py
@@ -47,22 +47,10 @@ while True:
     #Player
     player_gravity += 1
     player_rect.y += player_gravity
+    if player_rect.bottom >= 300: player_rect.bottom = 300
     screen.blit(player_surf, player_rect)
 
     screen.blit(player_surf,player_rect)
 
-
-
-    # keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-
-
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     pygame.mouse.get_pressed()
-        
     pygame.display.update()
     clock.tick(60)
