dfa39fc1e6c46c6aebd355c182ecfbb0e8a8dc98
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-14 11:16:11 +0200

    correct appearence of the obsticles

--- myGame.py
+++ myGame.py
@@ -14,7 +14,8 @@ def obstacle_movement(obstacle_list):
         for obstacle_rect in obstacle_list:
             obstacle_rect.x -= 5
 
-            screen.blit(snail_surf,obstacle_rect)
+            if obstacle_rect.bottom == 300: screen.blit(snail_surf,obstacle_rect)
+            else: screen.blit(fly_surf,obstacle_rect)
 
             obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
         return obstacle_list
