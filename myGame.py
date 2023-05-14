26eb132d7fd5fdf3d8d6d808aec660e2044344e0
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-14 10:22:26 +0200

    add game title

--- myGame.py
+++ myGame.py
@@ -32,6 +32,9 @@ player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alp
 player_stand = pygame.transform.rotozoom(player_stand,0,2)
 player_stand_rect = player_stand.get_rect(center = (400,200))
 
+game_name = test_font.render('Pixel Runner', False,(111,196,169))
+game_name_rect = game_name.get_rect(center = (400,80))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -79,6 +82,7 @@ while True:
     else:
         screen.fill((94,129,162))
         screen.blit(player_stand, player_stand_rect)
+        screen.blit(game_name,game_name_rect)
 
     pygame.display.update()
     clock.tick(60)
