ce3e6d914d74570f2276956b8a420b16a60959ae
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-15 09:07:27 +0200

    correct collision mechanics

--- myGame.py
+++ myGame.py
@@ -21,6 +21,12 @@ def obstacle_movement(obstacle_list):
         return obstacle_list
     else: return []
 
+def collisions(player, obstacles):
+    if obstacles:
+        for obstacle_rect in obstacles:
+            if player.colliderect(obstacle_rect): return False
+    return True
+
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
@@ -113,7 +119,7 @@ while True:
         obstacle_rect_list = obstacle_movement(obstacle_rect_list)
 
         #collisions
-
+        game_active = collisions(player_rect, obstacle_rect_list)
         
     else:
         screen.fill((94,129,162))
