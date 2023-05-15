a2797f21f8d0e1d91e4fc1bcbbce03aa1bade3e6
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-15 10:12:20 +0200

    bugfix - player walk animation

--- myGame.py
+++ myGame.py
@@ -60,11 +60,11 @@ fly_surf = pygame.image.load('graphics/fly/fly1.png').convert_alpha()
 
 obstacle_rect_list = []
 
-player_walk_1 = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
-player_walk_2 = pygame.image.load('graphics/Player/player_walk_2.png').convert_alpha()
-player_walk = [player_walk_2, player_walk_2]
+player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+player_walk = [player_walk_1, player_walk_2]
 player_index = 0
-player_jump = pygame.image.load('graphics/Player/jump.png').convert_alpha()
+player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
 
 player_surf = player_walk[player_index]
 player_rect = player_surf.get_rect(midbottom = (80,300))
