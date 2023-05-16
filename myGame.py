ced117436e8a3faff02d32bdffbd8a9664bc217d
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-16 09:01:25 +0200

    add all graphics to Player class

--- myGame.py
+++ myGame.py
@@ -5,7 +5,13 @@ from random import randint
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_walk_1 = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+        player_walk_2 = pygame.image.load('graphics/player/player_walk_2.png').convert_alpha()
+        self.player_walk = [player_walk_1, player_walk_2]
+        self.player_index = 0
+        self.player_jump = pygame.image.load('graphics/player/jump.png').convert_alpha()
+        
+        self.image = self.player_walk[self.player_index]
         self.rect = self.image.get_rect(midbottom = (200,300))
         self.gravity = 0
     
