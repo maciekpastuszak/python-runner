6525cb97bb12c4161577857d5d4cd4d0c1cbe0a1
Maciek Pastuszak <maciek.pastuszak@gmail.com>
2023-05-19 11:46:26 +0200

    add jumping sound

--- myGame.py
+++ myGame.py
@@ -14,11 +14,15 @@ class Player(pygame.sprite.Sprite):
         self.image = self.player_walk[self.player_index]
         self.rect = self.image.get_rect(midbottom = (80,300))
         self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound('audio/jump.mp3')
+        self.jump_sound.set_volume(0.05)
     
     def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.bottom >= 300:
             self.gravity = -20
+            self.jump_sound.play()
     
     def apply_gravity(self):
         self.gravity += 1
